063f222791861eadcd86b7ff92c76a4efc52c6f0
Pablo Santos <pablorasantos.dev@gmail.com>
2024-05-11 15:35:50 -0300

    feat: criando radio button acessivel

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -12,11 +12,11 @@ export function NewTransactionModal() {
         <input type="number" placeholder='Preço'/>
         <input type="text" placeholder='Categoria'/>
         <TransactionType>
-          <TransactionTypeButton variant='income'>
+          <TransactionTypeButton value='income' variant='income'>
             <ArrowCircleUp size={24}/>
             Entrada
           </TransactionTypeButton>
-          <TransactionTypeButton variant='outcome'>
+          <TransactionTypeButton value='outcome' variant='outcome'>
             <ArrowCircleDown size={24}/>
             Saída
           </TransactionTypeButton>
