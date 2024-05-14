c55e1c08444185f8dd6cac8bc2926526083c2b5d
Pablo Santos <pablorasantos.dev@gmail.com>
2024-05-13 21:01:35 -0300

    feat: aplicando react hook form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,16 +1,37 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const newTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  // type: z.enum(['income', 'outcome'])
+})
+
+type NewTransactionInput = z.infer<typeof newTransactionFormSchema>
 export function NewTransactionModal() {
+  const {register, handleSubmit, formState: {isSubmitting}} = useForm<NewTransactionInput>({
+    resolver: zodResolver(newTransactionFormSchema)
+  })
+
+  async function handleNewTransaction(data: NewTransactionInput){
+    await new Promise(resolve=> setTimeout(resolve, 2000))
+    console.log(data)
+  }
+
   return (
     <Dialog.Portal>
     <Overlay/>
     <Content>
       <Dialog.Title>Nova Transação</Dialog.Title>
-      <form>
-        <input type="text" placeholder='Descrição'/>
-        <input type="number" placeholder='Preço'/>
-        <input type="text" placeholder='Categoria'/>
+      <form onSubmit={handleSubmit(handleNewTransaction)}>
+        <input {...register('description')} type="text" placeholder='Descrição'/>
+        <input {...register('price', {valueAsNumber: true})} type="number" placeholder='Preço'/>
+        <input {...register('category')} type="text" placeholder='Categoria'/>
         <TransactionType>
           <TransactionTypeButton value='income' variant='income'>
             <ArrowCircleUp size={24}/>
@@ -21,7 +42,7 @@ export function NewTransactionModal() {
             Saída
           </TransactionTypeButton>
         </TransactionType>
-        <button type="submit">Cadastrar</button>
+        <button disabled={isSubmitting} type="submit">Cadastrar</button>
       </form>
       <CloseButton>
         <X/>
